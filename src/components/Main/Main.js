2d4b9bba5ea2325862b0d049780a20f572c225c8
Michael <michael.toepfer187@gmail.com>
2019-01-07 20:36:49 +0100

    second commit

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css';
+import Sprechblase from '../../pictures/Sprechblase2.png'
+import profil from '../../pictures/profpic.png'
+
+
+const Main = props => (
+  <div className="content">
+  bla
+  <img className="profil" src={profil} />
+  <img className="sprechblase" src={Sprechblase} />
+
+  </div>
+);
+
+export default Main;
